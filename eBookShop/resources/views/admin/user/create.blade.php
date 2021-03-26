55015e38d41d726538b42987346aa8d2a4ea5251
Rinrealvngangz <tuannmctk42@gmail.com>
2021-03-26 11:02:05 +0700

    add show create modal

--- eBookShop/resources/views/admin/user/create.blade.php
+++ eBookShop/resources/views/admin/user/create.blade.php
@@ -1,49 +1,57 @@
 @extends('layouts.main')
 @section('content')
 
-    <div class="container rounded bg-white">
-        <div id="msg">
 
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Create user</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
         </div>
-        <form>
+        <div class="modal-body">
+          <form>
             @csrf
-        @method('POST')
-            <div class="row">
-
-        <div class="p-3 py-3">
-            <div class="d-flex justify-content-between align-items-center mb-3">
-                <h4 class="text-right">Crate Users</h4>
-            </div>
-            <div class="row mt-3">
-
-                <div class="col-md-3">
-                    <label for="firstName" class="labels">First Name</label>
+            @method('POST')
+            <div class="form-group">
+                <label for="firstName" class="col-form-label">First Name</label>
                     <input name="firstName" id="firstName" type="text" class="form-control" placeholder="first name" value="">
-                </div>
-                <div class="col-md-6"><label for="lastName" class="labels">Last Name</label>
-                    <input name="lastName" id="lastName" type="text" class="form-control" value="" placeholder="lastname">
-                </div>
             </div>
-
-            <div class="row mt-3">
-                <div class="col-md-12"><label for="userName" class="labels">User name</label>
-                    <input name="userName" id="userName" type="text" class="form-control" placeholder="enter phone number" value=""></div>
-                <div class="col-md-12"><label for="email" class="labels">Email</label>
-                    <input name="email" id="email" type="text" class="form-control" placeholder="enter address" value=""></div>
-                <div class="col-md-12"><label for="password" class="labels">Password</label>
-                    <input name="password" id="password" type="password" class="form-control" placeholder="password" value=""></div>
-                <div class="col-md-12">
-                    <label for="password_confirmation" class="labels">Confirm Password</label>
-                    <input name="password_confirmation"  id="password_confirmation" type="password" class="form-control" placeholder="password confirm" value="">
-                </div>
+            <div class="form-group">
+                <label for="lastName" class="col-form-label">Last Name</label>
+                <input name="lastName" id="lastName" type="text" class="form-control" value="" placeholder="lastname">
             </div>
-
+            <div class="form-group">
+               <label for="email" class="col-form-label">Email</label>
+                    <input name="email" id="email" type="text" class="form-control" placeholder="enter address" value="">
+            </div>
+            <div class="form-group">
+                <label for="firstName" class="col-form-label">User Name</label>
+                <input name="userName" id="userName" type="text" class="form-control" placeholder="enter phone number" value="">
+            </div>
+            <div class="form-group">
+                <label for="password" class="col-form-label">Password</label>
+                <input name="password" id="password" type="password" class="form-control" placeholder="password" value="">
+            </div>
+            <div class="form-group">
+            <label for="password_confirmation" class="col-form-label">Confirm Password</label>
+            <input name="password_confirmation"  id="password_confirmation" type="password" class="form-control" placeholder="password confirm" value="">
+            </div>
+        </form>
         </div>
-            </div>
-            <div class="mt-5 text-center"><button class="btn btn-primary profile-button" type="submit" id="btn-submit">Create</button></div>
-            </form>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          <button type="button" id="btn-submit" class="btn btn-primary">Create user</button>
+        </div>
 
+       </div>
     </div>
+</div>
+
+
+
 
 
     @if(count($errors) >0)
@@ -78,7 +86,7 @@
                  },
                  success:function (data){
                      console.log(data.success);
-                     $("#msg").append( "<p>Successful Request!</p>" );
+                     $("#msg").append( `<p>${data.success}</p>` );
                      //alert(data.success);
                  },
                  error:function (data){
--- eBookShop/resources/views/admin/user/index.blade.php
+++ eBookShop/resources/views/admin/user/index.blade.php
@@ -71,9 +71,54 @@
               </tfoot>
             </table>
 
-              {!! Form::open(['method'=>'GET' , 'route' => ['user.create']]) !!}
-              {{ Form::button('Create User', ['class' => 'btn btn-primary', 'type' => 'submit']) }}
-              {!! Form::close() !!}
+              {{ Form::button('Create User', ['class' => 'btn btn-primary', 'type' => 'submit', 'data-toggle'=>'modal', 'data-target'=>'#exampleModal', 'data-whatever'=>'@mdo']) }}
+              <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h5 class="modal-title" id="exampleModalLabel">Create user</h5>
+                      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                      </button>
+                    </div>
+                    <div class="modal-body">
+                      <form>
+                        @csrf
+                        @method('POST')
+                        <div class="form-group">
+                            <label for="firstName" class="col-form-label">First Name</label>
+                                <input name="firstName" id="firstName" type="text" class="form-control" placeholder="first name" value="">
+                        </div>
+                        <div class="form-group">
+                            <label for="lastName" class="col-form-label">Last Name</label>
+                            <input name="lastName" id="lastName" type="text" class="form-control" value="" placeholder="lastname">
+                        </div>
+                        <div class="form-group">
+                           <label for="email" class="col-form-label">Email</label>
+                                <input name="email" id="email" type="text" class="form-control" placeholder="enter address" value="">
+                        </div>
+                        <div class="form-group">
+                            <label for="firstName" class="col-form-label">User Name</label>
+                            <input name="userName" id="userName" type="text" class="form-control" placeholder="enter phone number" value="">
+                        </div>
+                        <div class="form-group">
+                            <label for="password" class="col-form-label">Password</label>
+                            <input name="password" id="password" type="password" class="form-control" placeholder="password" value="">
+                        </div>
+                        <div class="form-group">
+                        <label for="password_confirmation" class="col-form-label">Confirm Password</label>
+                        <input name="password_confirmation"  id="password_confirmation" type="password" class="form-control" placeholder="password confirm" value="">
+                        </div>
+                    </form>
+                    </div>
+                    <div class="modal-footer">
+                      <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                      <button type="button" id="btn-submit" class="btn btn-primary">Create user</button>
+                    </div>
+
+                   </div>
+                </div>
+            </div>
           </div>
           <!-- /.card-body -->
         </div>
@@ -121,6 +166,16 @@
         "responsive": true,
       });
     });
+
+    $('#exampleModal').on('show.bs.modal', function (event) {
+  var button = $(event.relatedTarget) // Button that triggered the modal
+  var recipient = button.data('whatever') // Extract info from data-* attributes
+  // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+  // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+  var modal = $(this)
+  modal.find('.modal-title').text('New message to ' + recipient)
+ modal.find('.modal-body input').val(recipient)
+})
   </script>
 @endsection
 
