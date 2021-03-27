fde80d23a0a151f41839022118a56c18a0a9d1e6
Rinrealvngangz <tuannmctk42@gmail.com>
2021-03-27 16:24:10 +0700

    add modal form create user

--- eBookShop/resources/views/admin/user/index.blade.php
+++ eBookShop/resources/views/admin/user/index.blade.php
@@ -73,53 +73,8 @@
 
               {{ Form::button('Create User', ['class' => 'btn btn-primary', 'type' => 'submit', 'data-toggle'=>'modal', 'data-target'=>'#exampleModal']) }}
 
-                    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                        <div class="modal-dialog" role="document">
-                          <div class="modal-content">
-                            <div class="modal-header">
-                              <h5 class="modal-title" id="exampleModalLabel">Create user</h5>
-                              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                              </button>
-                            </div>
-                            <div class="modal-body">
-                              <form>
-                                @csrf
-                                @method('POST')
-                                <div class="form-group">
-                                    <label for="firstName" class="col-form-label">First Name</label>
-                                        <input name="firstName" id="firstName" type="text" class="form-control" placeholder="first name" value="">
-                                </div>
-                                <div class="form-group">
-                                    <label for="lastName" class="col-form-label">Last Name</label>
-                                    <input name="lastName" id="lastName" type="text" class="form-control" value="" placeholder="lastname">
-                                </div>
-                                <div class="form-group">
-                                   <label for="email" class="col-form-label">Email</label>
-                                        <input name="email" id="email" type="text" class="form-control" placeholder="enter address" value="">
-                                </div>
-                                <div class="form-group">
-                                    <label for="firstName" class="col-form-label">User Name</label>
-                                    <input name="userName" id="userName" type="text" class="form-control" placeholder="enter phone number" value="">
-                                </div>
-                                <div class="form-group">
-                                    <label for="password" class="col-form-label">Password</label>
-                                    <input name="password" id="password" type="password" class="form-control" placeholder="password" value="">
-                                </div>
-                                <div class="form-group">
-                                <label for="password_confirmation" class="col-form-label">Confirm Password</label>
-                                <input name="password_confirmation"  id="password_confirmation" type="password" class="form-control" placeholder="password confirm" value="">
-                                </div>
-                            </form>
-                            </div>
-                            <div class="modal-footer">
-                              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                              <button type="button" id="btn-submit" class="btn btn-primary">Create user</button>
-                            </div>
-
-                           </div>
-                        </div>
-                    </div>
+                 @include('admin.user.create')
+          </div>
 
           </div>
           <!-- /.card-body -->
@@ -168,6 +123,7 @@
         "responsive": true, "lengthChange": false, "autoWidth": false,
         "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
       }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+
       $('#example2').DataTable({
         "paging": true,
         "lengthChange": false,
@@ -178,16 +134,6 @@
         "responsive": true,
       });
     });
-
-   /* $('#exampleModal').on('show.bs.modal', function (event) {
-  var button = $(event.relatedTarget) // Button that triggered the modal
-  var recipient = button.data('whatever') // Extract info from data-* attributes
-  // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-  // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-  var modal = $(this)
-  modal.find('.modal-title').text('New message to ' + recipient)
- modal.find('.modal-body input').val(recipient)
-})*/
   </script>
 
 
@@ -204,13 +150,12 @@
                  "firstName":$('#firstName').val(),
                  "userName":$('#userName').val(),
                  "email":$('#email').val(),
-                 "password":$('#password').val(),
-                 "password_confirmation":$('#password_confirmation').val()
+                 "password":$('#password').val()
              },
              success:function (data){
                  console.log(data.success);
-                 $("#msg").append( `<p>${data.success}</p>` );
-                 //alert(data.success);
+                 $("#msg").append( `<p>${data.success}</p>`);
+                 setInterval('location.reload()', 1000);
              },
              error:function (data){
                  console.log('error');
@@ -220,7 +165,5 @@
      });
 
  </script>
-
-
 @endsection
 
