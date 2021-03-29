03c14ac26f7a4d90e85ba544b8218449b9f0f68c
Nguyen Mau Tuan <rinrealvngangz>
2021-03-29 17:49:29 +0700

    add design layout overview

--- eBookShop/resources/views/layouts/footer.blade.php
+++ eBookShop/resources/views/layouts/footer.blade.php
@@ -1,7 +1,25 @@
- <footer class="main-footer">
-          <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
-          All rights reserved.
-          <div class="float-right d-none d-sm-inline-block">
-            <b>Version</b> 3.1.0-rc
-          </div>
-        </footer>
+<footer class="footer footer-transparent d-print-none">
+    <div class="container">
+        <div class="row text-center align-items-center flex-row-reverse">
+            <div class="col-lg-auto ms-lg-auto">
+                <ul class="list-inline list-inline-dots mb-0">
+                    <li class="list-inline-item"><a href="./docs/index.html" class="link-secondary">Documentation</a></li>
+                    <li class="list-inline-item"><a href="./license.html" class="link-secondary">License</a></li>
+                    <li class="list-inline-item"><a href="https://github.com/tabler/tabler" target="_blank" class="link-secondary" rel="noopener">Source code</a></li>
+                </ul>
+            </div>
+            <div class="col-12 col-lg-auto mt-3 mt-lg-0">
+                <ul class="list-inline list-inline-dots mb-0">
+                    <li class="list-inline-item">
+                        Copyright &copy; 2021
+                        <a href="." class="link-secondary">Tabler</a>.
+                        All rights reserved.
+                    </li>
+                    <li class="list-inline-item">
+                        <a href="./changelog.html" class="link-secondary" rel="noopener">v1.0.0-beta</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
---
+++ eBookShop/resources/views/layouts/head.blade.php
@@ -0,0 +1,18 @@
+<head>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover"/>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
+    <title>Dashboard - Tabler - Premium and Open Source dashboard template with responsive and high quality UI.</title>
+    <!-- CSS files -->
+    <link href="/dist/css/tabler.min.css" rel="stylesheet"/>
+    <link href="/dist/css/tabler-flags.min.css" rel="stylesheet"/>
+    <link href="/dist/css/tabler-payments.min.css" rel="stylesheet"/>
+    <link href="/dist/css/tabler-vendors.min.css" rel="stylesheet"/>
+    <link href="/dist/css/demo.min.css" rel="stylesheet"/>
+    <link rel="stylesheet" href="/plugins/fontawesome-free/css/all.min.css">
+    <link rel="stylesheet" href="/dist/css/adminlte.min.css">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+
+    <!-- Theme style -->
+</head>
