92e2aaf748a5b592c018260413e90b9e475f5db0
Nguyen Trien <trienvaj@gmail.com>
2021-03-31 23:39:29 +0700

    finish category recusive

--- eBookShop/resources/views/layouts/sidebar.blade.php
+++ eBookShop/resources/views/layouts/sidebar.blade.php
@@ -49,7 +49,7 @@
 
 
                             <li  class="active" >
-                                <a class="sidenav-item-link" href="index.html">
+                                <a class="sidenav-item-link" href="{{route('category.index')}}">
                                     <span class="nav-text">Ecommerce</span>
 
                                 </a>
