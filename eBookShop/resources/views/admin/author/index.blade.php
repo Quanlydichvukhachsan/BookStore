a46250666cd4cd892f6b987ce71ddb0284ef5b30
Nguyen Trien <trienvaj@gmail.com>
2021-05-02 12:58:49 +0700

    create table author

---
+++ eBookShop/resources/views/admin/author/index.blade.php
@@ -0,0 +1,19 @@
+<table class="table">
+    <thead>
+    <tr>
+        <th scope="col">#</th>
+        <th scope="col">Họ và Tên Đệm</th>
+        <th scope="col">Tên</th>
+    </tr>
+    </thead>
+    <tbody>
+
+    <tr class="table-secondary">
+        <td scope="row">1</td>
+        <td>Nguyễn Đình Phát</td>
+        <td>Triển</td>
+    </tr>
+
+
+    </tbody>
+</table>
