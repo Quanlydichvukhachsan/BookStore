ba708125f1a7dd70596351e231a40e8dcde8bdcc
Rinrealvngangz <tuannmctk42@gmail.com>
2021-05-11 22:25:47 +0700

    ok discount book

---
+++ eBookShop/app/Models/Author.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Author extends Model
+{
+    use HasFactory;
+  protected $appends =['full_name'];
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'firstName',
+        'lastName',
+    ];
+
+    public  function books(){
+             $this->hasMany('App/Model/Book');
+    }
+
+    public function getFullNameAttribute(){
+      return  "{$this->lastName} {$this->firstName}";
+    }
+
+}
---
+++ eBookShop/app/Models/Order.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    protected $fillable=[
+        'state',
+        'active'
+    ];
+
+    // public function books(){
+    //      return $this->belongsToMany('App\Models\Book');
+    // }
+
+      public function user(){
+          return $this->belongsTo('App\Models\User');
+     }
+}
