47c6a697421f29b4c2dd8e6a8a88308d4c935548
Rinrealvngangz <tuannmctk42@gmail.com>
2021-05-20 11:20:56 +0700

    add view book

--- eBookShop/resources/views/app/layouts/header.blade.php
+++ eBookShop/resources/views/app/layouts/header.blade.php
@@ -11,7 +11,9 @@
         <div class="container">
             <ul class="navbar-nav pull-right">
                 <li class="nav-item active"><i class="fa fa-user"></i> <a href="http://127.0.0.1:8000/home" class="nav-link"> Home </a> </li>
-                <li class="nav-item"><i class="fa fa-star"></i> <a href="{{route('account',Auth::user()->id)}}" class="nav-link"> Account </a> </li>
+                @if(Auth::check())
+                    <li class="nav-item"><i class="fa fa-star"></i> <a href="{{route('account',Auth::user()->id)}}" class="nav-link"> Account </a> </li>
+                @endif
                 <li class="nav-item"><i class="fa fa-crosshairs"></i> <a href="{{route('register')}}" class="nav-link"> Register </a> </li>
                 <li class="nav-item"><i class="fa fa-shopping-cart"></i> <a href="{{route('cart')}}" class="nav-link notification"><span class="badge shopping-cart">0</span> <span>Cart</span> </a>  </li>
                 <li class="nav-item dropdown">
